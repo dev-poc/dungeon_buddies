a8c898c19f5d5c983fd414f8779f3d3e36d0f2ef
dev-poc <82632754+dev-poc@users.noreply.github.com>
2023-11-11 14:21:29 +1000

    Add files via upload

---
+++ gizmo.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+// Sphere gizmo to get attention.
+public class SphericalGizmo : MonoBehaviour
+{
+	[Header("Gizmo Settings")]
+	public Color gizmoColor = Color.white;
+	public float gizmoRadius = 0.5f;
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = gizmoColor;
+		Gizmos.DrawWireSphere(transform.position, gizmoRadius);
+		
+	}
+}
